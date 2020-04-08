71a50a688bdfdc14267fdd7c5c5fd665ff2352d2
Abhishek <abhishekamrutelfs@gmail.com>
2020-04-08 12:11:51 +0530

    file handling for image

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const tesseract = require("node-tesseract-ocr")
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const fs = require('fs');
+
+fs.writeFile("./test.text", "Hey there!", function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("The file was saved!");
+}); 
 
 
 app.use(express.json())
@@ -11,7 +19,10 @@ const config = {
   oem: 1,
   psm: 3,
 }
- 
+
+
+///  GET {{url}}/user
+
 app.get('/ocr',(req,res)=>
 {
   tesseract.recognize("image.jpg", config)
