dd3e3b18165d821527fb54f13b5f7646226c2878
Abhishek <abhishekamrutelfs@gmail.com>
2020-04-08 11:24:40 +0530

    Express server created

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const tesseract = require("node-tesseract-ocr")
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+
+app.use(express.json())
  
 const config = {
   lang: "eng",
@@ -6,10 +12,18 @@ const config = {
   psm: 3,
 }
  
-tesseract.recognize("image.jpg", config)
+app.get('/ocr',(req,res)=>
+{
+  tesseract.recognize("image.jpg", config)
   .then(text => {
     console.log("Result:", text)
+    res.send(text)
   })
   .catch(error => {
     console.log(error.message)
   })
+  
+})
+app.listen(port,()=>{
+    console.log('Server is up on port :'+port)
+})
