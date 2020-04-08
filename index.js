6ffad576e9bf43a5a8e671e00dccbe97ca256fc1
Abhishek <abhishekamrutelfs@gmail.com>
2020-04-08 11:06:12 +0530

    OCR integrated

---
+++ index.js
@@ -0,0 +1,15 @@
+const tesseract = require("node-tesseract-ocr")
+ 
+const config = {
+  lang: "eng",
+  oem: 1,
+  psm: 3,
+}
+ 
+tesseract.recognize("image.jpg", config)
+  .then(text => {
+    console.log("Result:", text)
+  })
+  .catch(error => {
+    console.log(error.message)
+  })
